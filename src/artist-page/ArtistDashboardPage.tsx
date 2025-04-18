60c4352d576d88bb30402f78b2382e89f5a1e9ff
Justyna Kwak <jk620@wp.pl>
2025-04-18 21:26:09 +0200

    Footer for the artist dashboard implemented

--- src/artist-page/ArtistDashboardPage.tsx
+++ src/artist-page/ArtistDashboardPage.tsx
@@ -1,9 +1,12 @@
 import { ArtistTopBar } from '../top-bars/ArtistTopBar';
+import { Footer } from '../footers/Footer';
 
 function ArtistDashboardPage() {
   return (
     <>
       <ArtistTopBar />
+
+      <Footer />
     </>
   );
 }
---
+++ src/footers/Footer.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Box,
+  Container,
+  Grid,
+  Link,
+  Typography,
+  IconButton,
+} from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import './Footer.css';
+
+export function Footer() {
+  return (
+    <Box className="footer">
+      <Container maxWidth="lg">
+        {/* Grid is deprecated but it doesn't change the functionality */}
+        <Grid
+          container
+          spacing={2}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={8}>
+            <Box className="footer-links">
+              <Link href="#" underline="none">
+                About
+              </Link>
+              <Link href="#" underline="none">
+                Features
+              </Link>
+              <Link href="#" underline="none">
+                Pricing
+              </Link>
+              <Link href="#" underline="none">
+                FAQ
+              </Link>
+              <Link href="#" underline="none">
+                Terms of Service
+              </Link>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} md={4} className="footer-icons">
+            <IconButton href="#" className="icon-btn">
+              <FacebookIcon fontSize="large" />
+            </IconButton>
+            <IconButton href="#" className="icon-btn">
+              <InstagramIcon fontSize="large" />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
