c448be2bf4f9c5da6c53f73f486a47d2ae8af745
Justyna Kwak <jk620@wp.pl>
2025-05-29 19:49:49 +0200

    fixed artist top bar so it looks more like customer's

--- src/top-bars/ArtistTopBar.tsx
+++ src/top-bars/ArtistTopBar.tsx
@@ -6,34 +6,49 @@ import {
   Box,
   Container,
   IconButton,
-  SxProps,
-  Theme,
   Toolbar,
   Typography,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
 } from '@mui/material';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import EmailIcon from '@mui/icons-material/Email';
+import Person2Icon from '@mui/icons-material/Person2';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Diversity2Icon from '@mui/icons-material/Diversity2';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export function ArtistTopBar() {
   const navigate = useNavigate();
-  const [inboxEmpty, setInboxEmpty] = useState(true); // TODO: if any requests are available, change to false
+  const [drawerOpened, setDrawerOpened] = useState(false);
+  const [inboxEmpty, setInboxEmpty] = useState(true); // TODO: Update when there are messages
 
-  const actions: {
-    icon: React.ReactElement;
-    action: () => void;
-    sx?: SxProps<Theme>;
-  }[] = [
+  const toggleDrawer = () => setDrawerOpened(!drawerOpened);
+
+  const actions = [
     {
       icon: (
-        <MailOutlineIcon
-          sx={{ fontSize: '2rem', color: inboxEmpty ? '' : '#25DAC5' }}
+        <EmailIcon
+          sx={{ fontSize: '2.2rem', color: inboxEmpty ? 'inherit' : '#25DAC5' }}
         />
       ),
+      label: 'notifications',
       action: () => navigate('/creator-dashboard'),
     },
     {
-      icon: <AccountCircleIcon sx={{ fontSize: '2rem' }} />,
+      icon: <Person2Icon sx={{ fontSize: '2.2rem' }} />,
+      label: 'profile',
       action: () => navigate('/profile-artist'),
+    },
+    {
+      icon: <LogoutIcon sx={{ fontSize: '2.2rem' }} />,
+      label: 'logout',
+      action: () => {
+        console.log('Logout artist');
+      },
     },
   ];
 
@@ -41,20 +56,62 @@ export function ArtistTopBar() {
     <>
       <AppBar sx={{ backgroundColor: 'white' }}>
         <Container maxWidth="xl">
-          <Toolbar className="artist-tool-bar">
-            <Typography variant="h1" component="div" className="title">
-              Craftable
-            </Typography>
-            <Box className="artist-toolbar-icons">
-              {actions.map(({ icon, action }, index) => (
-                <IconButton key={index} onClick={action}>
-                  {icon}
-                </IconButton>
-              ))}
+          <Toolbar className="customer-tool-bar">
+            <Box className="desktop-customer-tool-bar">
+              <Box className="modules-left">
+              <Diversity2Icon color="primary" fontSize="large" />
+              </Box>
+              <Box className="modules-right">
+                {actions.map((action) => (
+                  <IconButton key={action.label} onClick={action.action}>
+                    {action.icon}
+                  </IconButton>
+                ))}
+              </Box>
+            </Box>
+            <Box className="mobile-customer-tool-bar">
+              <IconButton
+                aria-label="open drawer"
+                edge="start"
+                onClick={toggleDrawer}
+                sx={{ p: 0 }}
+              >
+                <MenuIcon sx={{ fontSize: '2.2rem' }} />
+              </IconButton>
             </Box>
           </Toolbar>
         </Container>
       </AppBar>
+
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={drawerOpened}
+          onClose={toggleDrawer}
+          ModalProps={{ keepMounted: true }}
+          className="drawer-mobile"
+        >
+          <Box onClick={toggleDrawer} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+              Craftable
+            </Typography>
+            <Divider />
+            <List>
+              {actions.map((action) => (
+                <ListItem key={action.label} disablePadding>
+                  <ListItemButton
+                    sx={{ textAlign: 'center' }}
+                    onClick={action.action}
+                  >
+                    <ListItemText primary={action.label} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+      </nav>
+
       <Toolbar />
     </>
   );
